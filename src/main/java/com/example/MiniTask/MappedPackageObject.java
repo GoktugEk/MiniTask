9fb321bb4f56245e02831c439bf212190c133ad3
SH-Mapping-Package <goktug.ekinci@metu.edu.tr>
2021-11-29 22:22:17 +0300

    tabletransformation

---
+++ src/main/java/com/example/MiniTask/MappedPackageObject.java
@@ -0,0 +1,112 @@
+package com.example.MiniTask;
+
+
+public class MappedPackageObject{
+    private long id;
+    private String  createdAt;
+    private String lastUpdatedAt;
+    private int collectionDuration;
+    private int deliveryDuration;
+    private int eta;
+    private int leadTime;
+    private boolean orderInTime;
+
+
+
+    public MappedPackageObject() {
+        this.id = Long.parseLong(null);
+        this.orderInTime = Boolean.parseBoolean(null);
+        this.leadTime = Integer.parseInt(null);
+        this.eta = Integer.parseInt(null);
+        this.deliveryDuration = Integer.parseInt(null);
+        this.collectionDuration = Integer.parseInt(null);
+        this.lastUpdatedAt = null;
+        this.createdAt = null;
+    }
+
+    public MappedPackageObject(long id) {
+        this.id = id;
+        this.orderInTime = Boolean.parseBoolean(null);
+        this.leadTime = Integer.parseInt(null);
+        this.eta = Integer.parseInt(null);
+        this.deliveryDuration = Integer.parseInt(null);
+        this.collectionDuration = Integer.parseInt(null);
+        this.lastUpdatedAt = null;
+        this.createdAt = null;
+    }
+
+    public MappedPackageObject(long id, String createdAt, String lastUpdatedAt, int collectionDuration, int deliveryDuration, int eta, int leadTime, boolean orderInTime){
+        this.id = id;
+        this.orderInTime = orderInTime;
+        this.leadTime = leadTime;
+        this.eta = eta;
+        this.deliveryDuration = deliveryDuration;
+        this.collectionDuration = collectionDuration;
+        this.lastUpdatedAt = lastUpdatedAt;
+        this.createdAt = createdAt;
+
+    }
+
+    public int getCollectionDuration() {
+        return collectionDuration;
+    }
+
+    public int getDeliveryDuration() {
+        return deliveryDuration;
+    }
+
+    public int getEta() {
+        return eta;
+    }
+
+    public int getLeadTime() {
+        return leadTime;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getLastUpdatedAt() {
+        return lastUpdatedAt;
+    }
+
+    public void setCollectionDuration(int collectionDuration) {
+        this.collectionDuration = collectionDuration;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setDeliveryDuration(int deliveryDuration) {
+        this.deliveryDuration = deliveryDuration;
+    }
+
+    public void setEta(int eta) {
+        this.eta = eta;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setLastUpdatedAt(String lastUpdatedAt) {
+        this.lastUpdatedAt = lastUpdatedAt;
+    }
+
+    public void setLeadTime(int leadTime) {
+        this.leadTime = leadTime;
+    }
+
+    public void setOrderInTime(boolean orderInTime) {
+        this.orderInTime = orderInTime;
+    }
+}
+
+
+
---
+++ src/main/java/com/example/MiniTask/TableTransformation.java
@@ -0,0 +1,101 @@
+package com.example.MiniTask;
+
+import org.springframework.transaction.TransactionUsageException;
+
+import java.io.*;
+import java.util.Scanner;
+
+
+
+public class TableTransformation {
+
+    public static void main(String[] args) throws IOException {
+
+        ///Users/macbookretina/MiniTask/consumerRecords.out
+
+        File file = new File("/Users/macbookretina/MiniTask/consumerRecords.out");
+
+        Scanner scan = new Scanner(file);
+
+        String fileContent = "";
+        while(scan.hasNextLine()){
+            String oldLine = scan.nextLine();
+            long id;
+            String  createdAt;
+            String lastUpdatedAt;
+            int collectionDuration;
+            int deliveryDuration;
+            int eta;
+            int leadTime;
+            boolean orderInTime;
+
+            //ID
+            int indexIn = oldLine.indexOf("\"id\"");
+            String sid = oldLine.substring(indexIn+5,indexIn+13);
+            id = Long.parseLong(sid);
+
+            //CREATED_AT
+            int indexCreat = oldLine.indexOf("\"created_at\"");
+            createdAt = oldLine.substring(indexCreat+14,indexCreat+40);
+
+            //LAST_UPDATED_AT
+            int indexLast = oldLine.indexOf("\"last_updated_at\"");
+            lastUpdatedAt = oldLine.substring(indexLast+19,indexLast+45);
+
+            //COLLECTION_DURATION
+            int indexCollect = oldLine.indexOf("\"collected_at\"");
+            int indexAssign = oldLine.indexOf("\"assigned_at\"");
+            String scollect = oldLine.substring(indexCollect+16,indexCollect+42);
+            String sassign = oldLine.substring(indexAssign+16,indexAssign+42);
+        /*    int icollect = Integer.parseInt(scollect);
+            int iassign = Integer.parseInt(sassign);
+            collectionDuration = icollect - iassign; */
+            collectionDuration=1;
+            System.out.println(scollect+" mmmm "+ sassign);
+
+            //DELIVERY_DURATION
+            int indexDeliver = oldLine.indexOf("\"delivery_date\"");
+            String sdeliver = oldLine.substring(indexDeliver+16,indexDeliver+42);
+        /*    int ideliver = Integer.parseInt(sdeliver);
+            deliveryDuration = ideliver - icollect;*/
+            deliveryDuration=1;
+        //    System.out.println(sdeliver+" mmmm "+ scollect);
+
+            //ETA
+            int indexEta = oldLine.indexOf("\"eta\"");
+            int indexEtalast = oldLine.indexOf("\"eta_for_prep\"");
+            String seta = oldLine.substring(indexEta+6,indexEtalast-1);
+            System.out.println(seta);
+            //if(seta == "null")
+            eta = 0;
+            //else eta = Integer.parseInt(seta);
+
+            //LEAD_TIME
+        //    int indexLead = oldLine.indexOf("\"collect\"");
+        //    String slead = oldLine.substring(indexLead+5,indexLead+13);
+        //    leadTime = Integer.parseInt(slead);
+            leadTime=1;
+
+            //ORDER_IN_TIME
+        //    int indexOrder = oldLine.indexOf("\"collect\"");
+        //    String sorder = oldLine.substring(indexOrder+5,indexOrder+13);
+        //    orderInTime = true;
+            orderInTime=true;
+
+            MappedPackageObject newLine = new MappedPackageObject(id,createdAt,lastUpdatedAt,collectionDuration,deliveryDuration,eta,leadTime,orderInTime);
+
+            String newiLine = "\"data\""+"{\"id\":"+id+","+"\"created_at\":"+createdAt+","+"\"last_updated_at\":"+lastUpdatedAt+","+"\"collection_duration\":"+collectionDuration+","+"\"delivery_duration\":"+deliveryDuration+","+"\"eta\":"+eta+","+"\"lead_time\":"+leadTime+"\"order_in_time\":"+orderInTime+"}";
+
+            fileContent = fileContent.concat(newiLine + "\n");
+            System.out.println(newiLine);
+        }
+
+        FileWriter writer = new FileWriter("/Users/macbookretina/MiniTask/mappedRecords.out");
+        writer.write(fileContent);
+
+        writer.close();
+    }
+
+
+
+}
