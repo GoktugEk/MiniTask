b5147bd0e6535819de056ffb37823e78bc9008e0
GoktugEk <goktug.ekinci@metu.edu.tr>
2021-12-01 01:05:07 +0300

    Comment Addition and Code Organisation

--- src/main/java/com/example/MiniTask/TableTransformation.java
+++ src/main/java/com/example/MiniTask/TableTransformation.java
@@ -2,42 +2,29 @@ package com.example.MiniTask;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import com.google.gson.Gson;
-import org.springframework.boot.json.GsonJsonParser;
-import org.springframework.boot.json.JsonParser;
-import org.springframework.transaction.TransactionUsageException;
 
 import java.io.*;
-import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
-import static com.fasterxml.jackson.databind.MapperFeature.DEFAULT_VIEW_INCLUSION;
 
 
 public class TableTransformation {
     public static void main(String[] args) throws IOException, ParseException {
 
-        ///Users/macbookretina/MiniTask/consumerRecords.out
-
         File file = new File(System.getProperty("user.dir") + "/consumerRecords.out");
-
         Scanner scan = new Scanner(file);
 
         String fileContent = "";
         String oldLine = scan.nextLine();
 
-
-
-        int counter = 0;
         while(scan.hasNextLine()){
-
             List packageList = new ArrayList();
             int len = 0;
 
-            ObjectMapper MAPPER = new ObjectMapper().disable(DEFAULT_VIEW_INCLUSION).disable(FAIL_ON_UNKNOWN_PROPERTIES);
+            ObjectMapper MAPPER = new ObjectMapper().disable(FAIL_ON_UNKNOWN_PROPERTIES);
             Package pac  = MAPPER.readValue(oldLine, Package.class);
 
             if ((! "package".equals(pac.table) ) && scan.hasNextLine()) {
@@ -45,11 +32,11 @@ public class TableTransformation {
                 continue;
             }
 
-            //ID THIS IS COMPLETED
+            //ID
             long id = pac.xid;
             long temp = id;
 
-            while( id == temp){
+            while( id == temp){ //Getting all the updates on the same package to a list
                 packageList.add(pac);
                 if(scan.hasNextLine()) oldLine = scan.nextLine();
                 else break;
@@ -58,25 +45,20 @@ public class TableTransformation {
                 len++;
             }
 
-
-
             String  createdAt;
             String lastUpdatedAt;
             int collectionDuration = 0;
             int deliveryDuration = 0;
-            int eta = 0;
+            int eta;
             int leadTime = 0;
             boolean orderInTime;
 
-            Date icollect = null, iassign = null, ideliver = null, ilead1 = null, ilead2 = null;
+            Date icollect = null, iassign = null, ideliver = null, ilead1 = null, ilead2 = null; //DEFAULT PARAMETER VALUES
 
 
-            pac = (Package) packageList.get(len-1);
+            pac = (Package) packageList.get(len-1); //PACKAGE OBJECT BELONGS TO THE LAST LINE OF REGARDING PACKAGE IN THE OLD MESSAGES
 
-
-
-
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"); //DATE PARSING
             if(pac.data.collected_at != null) icollect = format.parse(pac.data.collected_at);
             if(pac.data.assigned_at != null) iassign = format.parse(pac.data.assigned_at);
             if(pac.data.in_delivery_at != null)ideliver = format.parse(pac.data.in_delivery_at);
@@ -84,14 +66,10 @@ public class TableTransformation {
             if(pac.data.completed_at != null)ilead1 = format.parse(pac.data.completed_at);
             if(pac.data.created_at != null)ilead2 = format.parse(pac.data.created_at);
 
-
-
-            //CREATED_AT THIS IS COMPLETED
-
+            //CREATED_AT
             createdAt = pac.data.created_at;
 
-
-            //LAST_UPDATED_AT THIS IS COMPLETED
+            //LAST_UPDATED_AT
             lastUpdatedAt = pac.data.last_updated_at;
 
             //COLLECTION_DURATION
@@ -103,10 +81,8 @@ public class TableTransformation {
             //DELIVERY_DURATION
             if(icollect != null && ideliver != null) deliveryDuration = ((int) (ideliver.getTime() - icollect.getTime()))/ 1000;
 
-
             //LEAD_TIME
             if(ilead1 != null && ilead2 != null) leadTime = ((int) (ilead1.getTime() - ilead2.getTime()))/ 1000;
-
 
             //ORDER_IN_TIME
             if(deliveryDuration+collectionDuration<=eta) orderInTime = true;
@@ -117,10 +93,8 @@ public class TableTransformation {
             ObjectWriter writer = new ObjectMapper().writer().withDefaultPrettyPrinter();
             String json = writer.writeValueAsString(newLine);
 
-            fileContent = fileContent.concat(json);
+            fileContent = fileContent.concat(json); //NEW LINE ADDED TO THE FILE CONTENT
             System.out.println(json);
-            counter++;
-            System.out.println(counter);
         }
 
         FileWriter writer = new FileWriter(System.getProperty("user.dir") + "/mappedRecords.out");
